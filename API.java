4979ecf8d7c32b298b5b2dd0e32a63f418ea0974
DjStonie <valdonay@yahoo.dk>
2017-05-17 11:24:08 +0200

    Create API.java

---
+++ API.java
@@ -0,0 +1,358 @@
+ public class API {
+ 
+ public static void Rollanimation(String s){
+		if (s.length()<72){
+			String before = "#";
+			String after = "#";
+			int i = 70-s.length();
+			int i2 = i/2;
+			while (before.length()!=i2){
+				before = before + " ";
+			}
+			while (after.length()!=i2){
+				after = " "+after;
+			}
+			if (i % 2 == 1){
+				before = before + " ";
+			}
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("######################################################################");
+			System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println(before+s+after);
+			System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println("#                                                                    #");
+			System.out.println("######################################################################");
+         System.out.println();
+		}else{
+			System.out.println("Der er sket en fejl");
+		}
+	}
+   
+   public static void Rollanimation(String s,String s2){
+		if (s.length()<72&&s2.length()<72){
+			String before = "#";
+			String after = "#";
+			int i = 70-s.length();
+			int i2 = i/2;
+			while (before.length()!=i2){
+				before = before + " ";
+			}
+			while (after.length()!=i2){
+				after = " "+after;
+			}
+			if (i % 2 == 1){
+				before = before + " ";
+			}
+         String before2 = "#";
+			String after2 = "#";
+			int i21 = 70-s2.length();
+			int i22 = i21/2;
+			while (before2.length()!=i22){
+				before2 = before2 + " ";
+			}
+			while (after2.length()!=i22){
+				after2 = " "+after2;
+			}
+			if (i21 % 2 == 1){
+				before2 = before2 + " ";
+			}
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("######################################################################");
+			System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println(before+s+after);
+			System.out.println(before2+s2+after2);
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println("#                                                                    #");
+			System.out.println("######################################################################");
+         System.out.println();
+		}else{
+			System.out.println("Der er sket en fejl");
+		}
+	}
+   
+   public static void Rollanimation(String s,String s2,String s3){
+		if (s.length()<72&&s2.length()<72){
+			String before = "#";
+			String after = "#";
+			int i = 70-s.length();
+			int i2 = i/2;
+			while (before.length()!=i2){
+				before = before + " ";
+			}
+			while (after.length()!=i2){
+				after = " "+after;
+			}
+			if (i % 2 == 1){
+				before = before + " ";
+			}
+         String before2 = "#";
+			String after2 = "#";
+			int i21 = 70-s2.length();
+			int i22 = i21/2;
+			while (before2.length()!=i22){
+				before2 = before2 + " ";
+			}
+			while (after2.length()!=i22){
+				after2 = " "+after2;
+			}
+			if (i21 % 2 == 1){
+				before2 = before2 + " ";
+			}
+         String before3 = "#";
+			String after3 = "#";
+			int i211 = 70-s3.length();
+			int i221 = i211/2;
+			while (before3.length()!=i221){
+				before3 = before3 + " ";
+			}
+			while (after3.length()!=i221){
+				after3 = " "+after3;
+			}
+			if (i211 % 2 == 1){
+				before3 = before3 + " ";
+			}
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("######################################################################");
+			System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println(before+s+after);
+			System.out.println(before2+s2+after2);
+         System.out.println(before3+s3+after3);
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println("#                                                                    #");
+			System.out.println("######################################################################");
+         System.out.println();
+		}else{
+			System.out.println("Der er sket en fejl");
+		}
+	}
+  
+     public static void Rollanimation(String s,String s2,String s3,String s4){
+		if (s.length()<72&&s2.length()<72&&s3.length()<72&&s4.length()<72){
+			String before = "#";
+			String after = "#";
+			int i = 70-s.length();
+			int i2 = i/2;
+			while (before.length()!=i2){
+				before = before + " ";
+			}
+			while (after.length()!=i2){
+				after = " "+after;
+			}
+			if (i % 2 == 1){
+				before = before + " ";
+			}
+         String before2 = "#";
+			String after2 = "#";
+			int i21 = 70-s2.length();
+			int i22 = i21/2;
+			while (before2.length()!=i22){
+				before2 = before2 + " ";
+			}
+			while (after2.length()!=i22){
+				after2 = " "+after2;
+			}
+			if (i21 % 2 == 1){
+				before2 = before2 + " ";
+			}
+         String before3 = "#";
+			String after3 = "#";
+			int i211 = 70-s3.length();
+			int i221 = i211/2;
+			while (before3.length()!=i221){
+				before3 = before3 + " ";
+			}
+			while (after3.length()!=i221){
+				after3 = " "+after3;
+			}
+			if (i211 % 2 == 1){
+				before3 = before3 + " ";
+			}
+      
+      
+         String before4 = "#";
+			String after4 = "#";
+			int i4 = 70-s4.length();
+			int i5 = i4/2;
+			while (before4.length()!=i5){
+				before4 = before4 + " ";
+			}
+			while (after4.length()!=i5){
+				after4 = " "+after4;
+			}
+			if (i5 % 2 == 1){
+				before4 = before4 + " ";
+			}
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("######################################################################");
+			System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println(before+s+after);
+			System.out.println(before2+s2+after2);
+         System.out.println(before3+s3+after3);
+         System.out.println(before4+s4+after4);
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println("#                                                                    #");
+			System.out.println("######################################################################");
+         System.out.println();
+		}else{
+			System.out.println("Der er sket en fejl");
+		}
+	}
+  
+       public static void Rollanimation(String s,String s2,String s3,String s4,String s5){
+		if (s.length()<72&&s2.length()<72&&s3.length()<72&&s4.length()<72&&s5.length()<72){
+			String before = "#";
+			String after = "#";
+			int i = 70-s.length();
+			int i2 = i/2;
+			while (before.length()!=i2){
+				before = before + " ";
+			}
+			while (after.length()!=i2){
+				after = " "+after;
+			}
+			if (i % 2 == 1){
+				before = before + " ";
+			}
+         String before2 = "#";
+			String after2 = "#";
+			int i21 = 70-s2.length();
+			int i22 = i21/2;
+			while (before2.length()!=i22){
+				before2 = before2 + " ";
+			}
+			while (after2.length()!=i22){
+				after2 = " "+after2;
+			}
+			if (i21 % 2 == 1){
+				before2 = before2 + " ";
+			}
+         String before3 = "#";
+			String after3 = "#";
+			int i211 = 70-s3.length();
+			int i221 = i211/2;
+			while (before3.length()!=i221){
+				before3 = before3 + " ";
+			}
+			while (after3.length()!=i221){
+				after3 = " "+after3;
+			}
+			if (i211 % 2 == 1){
+				before3 = before3 + " ";
+			}
+      
+      
+         String before4 = "#";
+			String after4 = "#";
+			int i4 = 70-s4.length();
+			int i5 = i4/2;
+			while (before4.length()!=i5){
+				before4 = before4 + " ";
+			}
+			while (after4.length()!=i5){
+				after4 = " "+after4;
+			}
+			if (i5 % 2 == 1){
+				before4 = before4 + " ";
+			}
+      
+         String before5 = "#";
+			String after5 = "#";
+			int i41 = 70-s5.length();
+			int i51 = i41/2;
+			while (before5.length()!=i51){
+				before5 = before5 + " ";
+			}
+			while (after5.length()!=i51){
+				after5 = " "+after5;
+			}
+			if (i51 % 2 == 1){
+				before5 = before5 + " ";
+			}
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("######################################################################");
+			System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println(before+s+after);
+			System.out.println(before2+s2+after2);
+         System.out.println(before3+s3+after3);
+         System.out.println(before4+s4+after4);
+         System.out.println(before5+s5+after5);
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+         System.out.println("#                                                                    #");
+			System.out.println("#                                                                    #");
+			System.out.println("######################################################################");
+         System.out.println();
+		}else{
+			System.out.println("Der er sket en fejl");
+		}
+	}
+}
