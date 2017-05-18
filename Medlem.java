3d0bf16ccfa1513d1156fd60547ae9c725a8f397
AStonor <lampefeber@hotmail.com>
2017-05-18 19:55:41 +0200

    Update Medlem.java

--- Medlem.java
+++ Medlem.java
@@ -1,8 +1,5 @@
-package com.company;
 
-/**
- * Created by lampe on 16-05-2017.
- */
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
@@ -16,7 +13,7 @@ public class Medlem  implements java.io.Serializable {
     private String Vejnavn;
     private String Vejnummer;
     private int Postnummer;
-    private String By
+    private String by;
     private int Telefonnummer;
     private Date SidsteBetalingsdato;
     private boolean MedlemsStatus;
@@ -26,141 +23,137 @@ public class Medlem  implements java.io.Serializable {
     private boolean Træner;
 
     //her kommer get metoderne
-    public String getID()
-    {
+    public String getID() {
         return ID;
     }
-    public int getKode()
-    {
+
+    public int getKode() {
         return Kode;
     }
-    public String getFornavn()
-    {
+
+    public String getFornavn() {
         return Fornavn;
     }
-    public String getEfternavn()
-    {
+
+    public String getEfternavn() {
         return Efternavn;
     }
-    public String getFødselsdato()
-    {
+
+    public String getFødselsdato() {
         return Fødselsdato;
     }
-    public String getVejnavn()
-    {
+
+    public String getVejnavn() {
         return Vejnavn;
     }
-    public String getVejnummer()
-    {
+
+    public String getVejnummer() {
         return Vejnummer;
     }
-    public int getPostnummer()
-    {
+
+    public int getPostnummer() {
         return Postnummer;
     }
-    public String getBy
-    {
-        return By
-    }
-    public int getTelefonnummer()
-    {
+
+    public int getTelefonnummer() {
         return Telefonnummer;
     }
-    public Date getSidsteBetalingsdato()
-    {
+
+    public Date getSidsteBetalingsdato() {
         return SidsteBetalingsdato;
     }
-    public boolean getMedlemsStatus()
-    {
+
+    public boolean getMedlemsStatus() {
         return MedlemsStatus;
     }
-    public boolean getCheckKvinde()
-    {
+
+    public boolean getCheckKvinde() {
         return CheckKvinde;
     }
-    public boolean getFormand()
-    {
+
+    public boolean getFormand() {
         return Formand;
     }
-    public boolean getBogholder()
-    {
+
+    public boolean getBogholder() {
         return Bogholder;
     }
-    public boolean getTræner()
-    {
+
+    public boolean getTræner() {
         return Træner;
     }
 
 /* her kommer set metoderne */
 
-    public void setID(String id)
-    {
+    public void setID(String id) {
         ID = id;
     }
-    public void setKode(int kode)
-    {
+
+    public void setBy(String by){
+        this.by = by;
+    }
+
+    public void setKode(int kode) {
         Kode = kode;
     }
-    public void setFornavn(String fornavn)
-    {
+
+    public void setFornavn(String fornavn) {
         Fornavn = fornavn;
     }
-    public void setEfternavn(String efternavn)
-    {
+
+    public void setEfternavn(String efternavn) {
         Efternavn = efternavn;
     }
-    public void setFødselsdato(String fødselsdato)
-    {
+
+    public void setFødselsdato(String fødselsdato) {
         Fødselsdato = fødselsdato;
     }
-    public void setVejnavn(String vejnavn)
-    {
+
+    public void setVejnavn(String vejnavn) {
         Vejnavn = vejnavn;
     }
-    public void setVejnummer(String vejnummer)
-    {
+
+    public void setVejnummer(String vejnummer) {
         Vejnummer = vejnummer;
     }
-    public void setPostnummer(int postnummer)
-    {
+
+    public void setPostnummer(int postnummer) {
         Postnummer = postnummer;
     }
-    public void setBy(String by)
-    {
-        By = by;
-    }
-    public void setTelefonnummer(int telefonnummer)
-    {
+
+    public void setTelefonnummer(int telefonnummer) {
         Telefonnummer = telefonnummer;
     }
-    public void setSidsteBetalingsdato(Date sidstebetalingsdato)
-    {
+
+    public void setSidsteBetalingsdato(Date sidstebetalingsdato) {
         SidsteBetalingsdato = sidstebetalingsdato;
     }
-    public void setMedlemsStatus(boolean medlemsstatus)
-    {
+
+    public void setMedlemsStatus(boolean medlemsstatus) {
         MedlemsStatus = medlemsstatus;
     }
-    public void setCheckKvinde(boolean checkkvinde)
-    {
+
+    public void setCheckKvinde(boolean checkkvinde) {
         CheckKvinde = checkkvinde;
     }
 
-    public void setFormand(boolean formand)
-    {
+    public void setFormand(boolean formand) {
         Formand = formand;
     }
-    public void setBogholder(boolean bogholder)
-    {
+
+    public void setBogholder(boolean bogholder) {
         Bogholder = bogholder;
     }
-    public void setTræner(boolean træner)
-    {
+
+    public void setTræner(boolean træner) {
         Træner = træner;
     }
 
-    public Medlem(int antalMedlemmer, String fornavn, String efternavn, String fødselsdato, String vejnavn, String vejnummer, int postnummer, String by, int telefonnummer, boolean checkkvinde)
-    {
+    public Medlem(int antalMedlemmer, String fornavn, String efternavn, String fødselsdato, String vejnavn, String vejnummer, int postnummer, String by, int telefonnummer, boolean checkkvinde) {
+        Formand = false;
+        Bogholder = false;
+        Træner = false;
+        MedlemsStatus = true;
         ID = genID(antalMedlemmer);
         Kode = genKode();
         Fornavn = fornavn;
@@ -169,6 +162,7 @@ public class Medlem  implements java.io.Serializable {
         Vejnavn = vejnavn;
         Vejnummer = vejnummer;
         Postnummer = postnummer;
+        this.by = by;
         Telefonnummer = telefonnummer;
         CheckKvinde = checkkvinde;
         Date nu = new Date();
@@ -179,34 +173,62 @@ public class Medlem  implements java.io.Serializable {
 
 /* her genereres en kode for brugeren mellem 1000 og 9999 */
 
-    public int genKode()
-    {
+    public int genKode() {
         Random r = new Random();
-        return (r.nextInt(8999)+1000);
+        return (r.nextInt(8999) + 1000);
     }
 
 /* Her tilføjes bogstav til ID der indikere typen af bruger som enten formand, bogholder, træner eller medlem*/
 
-    public String genID(int antalMedlemmer)
-    {
+    public String genID(int antalMedlemmer) {
         String genID2 = "";
-        if(Formand)
-        {
+        if (Formand) {
             genID2 += "F";
-        }
-        else if(Bogholder)
-        {
+        } else if (Bogholder) {
             genID2 += "B";
-        }
-        else if(Træner)
-        {
+        } else if (Træner) {
             genID2 += "T";
-        }
-        else
-        {
+        } else {
             genID2 += "M";
         }
         genID2 += (antalMedlemmer + 1);
         return genID2;
+    }
+
+    public String toString() {
+        String s = "\n";
+
+        s += Fornavn + " " + Efternavn + "\n";
+        s += Vejnavn + " " + Vejnummer + "\n";
+        s += Postnummer + " " + by + "\n";
+        s += "Tlf nr.: " + Telefonnummer + "\n";
+        s += "\n";
+        if(Bogholder){
+            s += "Bogholder";
+        }
+        else if(Formand){
+            s += "Formand";
+        }
+        else if(Træner){
+            s += "Træner";
+        }
+        else{
+            s += "Medlem";
+
+        }
+        s += "\n";
+        s += "ID: " + ID + "\n";
+        if (MedlemsStatus) {
+            s += "Status: Aktiv";
+        } else {
+            s += "Status: Ikke aktiv";
+        }
+        s += "\n";
+        s += "Fødselsdato: " + Fødselsdato  + "\n";
+
+        SimpleDateFormat ft =  new SimpleDateFormat ("dd-MM-yyyy");
+        s += "Oprettelsesdato: " + ft.format(SidsteBetalingsdato) + "\n";
+
+        return s;
     }
 }
